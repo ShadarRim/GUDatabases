5ba02c0108cf91abd6e16aafa6310602b4a0f156
Alexey Egorov <egorovalexeyd@gmail.com>
2020-01-20 10:54:49 +0300

    lesson #6 fixed task #3

--- 6/3.sql
+++ 6/3.sql
@@ -20,11 +20,23 @@ SELECT target_id, COUNT(*) as count_likes FROM likes WHERE target_type_id = 2 GR
 SELECT target_id as target, COUNT(*) as count_likes FROM likes WHERE target_type_id = 2 GROUP BY target_id;
 
 
--- финальный запрос
+-- финальный запрос №1
 SELECT SUM(tab2.likes_count) FROM
 	(SELECT subtab.target_id, subtab.bd, COUNT(*) as likes_count FROM
-		(SELECT id, target_id, (SELECT birthday FROM profiles WHERE profiles.user_id = target_id) as bd FROM likes) AS subtab
+		(SELECT id, target_id, (SELECT birthday FROM profiles WHERE profiles.user_id = target_id) as bd FROM likes WHERE target_type_id = 2) AS subtab
 	GROUP BY subtab.target_id
 	ORDER BY subtab.bd DESC
 	LIMIT 10) as tab2
 ;
+
+-- ещё вариант №2
+SELECT target_id, COUNT(*) as count_likes FROM likes WHERE target_type_id = 2 AND target_id IN
+(SELECT * FROM
+	(SELECT user_id FROM profiles ORDER BY birthday DESC LIMIT 10) as tab)
+GROUP BY target_id;
+
+-- обратите внимание, что ответы не сходятся
+-- всё дело в том, что первый запрос считает лайки, у пользователей, а затем сортирует по возрасту
+-- второй же запрос сначала находит топ-10 по возрасту, потому считает их лайки (точнее оставляет)
+-- в итоге по второму запросу есть пользователи, который не в топ-10 попали, но лайков не получили
+-- а в первом запросе топ-10 из тех кто получил лайки
