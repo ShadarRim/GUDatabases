eb0503c2ed2d9d86d670b859fcace08f24016be7
Alexey Egorov <egorovalexeyd@gmail.com>
2020-01-22 23:28:29 +0300

    HW6 fix #2

--- 6/5.sql
+++ 6/5.sql
@@ -7,11 +7,11 @@ SHOW TABLES;
 
 SELECT user_id, SUM(num) as sumnum FROM (
 	SELECT id as user_id, 0 as num from users
-	UNION
+	UNION ALL
 	SELECT user_id, COUNT(*) as num FROM likes GROUP BY user_id
-	UNION
+	UNION ALL
 	SELECT from_user_id as user_id, COUNT(*) as num FROM messages GROUP BY from_user_id
-	UNION
+	UNION ALL
 	SELECT user_id, COUNT(*) as num FROM media GROUP BY user_id) AS subtub
 GROUP BY user_id
 ORDER BY sumnum
